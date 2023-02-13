8cb04d63f9625b38ceede716c2d191d0f047fc26
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-13 22:53:48 +0530

    Holding Current Score

--- script.js
+++ script.js
@@ -19,12 +19,21 @@ diceEl.classList.add('hidden');
 const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer===0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function(){
+    if(playing){
     //1.Generating a random dice roll
     const dice = Math.floor(Math.random() * 6) +1;
-console.log(dice);
     //2.Display Dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
@@ -38,10 +47,29 @@ document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 }
 else{
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer===0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
 }
+    }
+})
+
+btnHold.addEventListener('click', function( ){
+    if(playing){
+    //1. Add current score to active players score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    //2.Check if players score >= 100
+
+    if(scores[activePlayer] >= 30){
+        //Finish the game
+
+        playing = false;
+        diceEl.classList.add('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');    
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');    
+}
+else{
+    //Switch to next player
+    switchPlayer();
+}
+    }
 })
